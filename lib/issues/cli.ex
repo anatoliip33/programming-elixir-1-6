7d0b92edfd66dceb827847ef26212815a27a1bd7
polishchuk <anatolii.polishchuk@ria.com>
2023-09-27 23:47:35 +0300

    fetch data from github

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,7 +8,41 @@ defmodule Issues.CLI  do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args()
+    |> process()
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort_by(& &1["created_at"], :desc)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
+  end
+
+  def decode_response({:ok, body}), do: body
+  
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
   end
 
   @doc """
--- mix.exs
+++ mix.exs
@@ -6,6 +6,9 @@ defmodule Issues.MixProject do
       app: :issues,
       version: "0.1.0",
       elixir: "~> 1.14",
+      elixirc_options: [
+        warnings_as_errors: true
+      ],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -22,7 +25,9 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 2.1"}
+      {:httpoison, "~> 2.1"},
+      {:poison, "~> 5.0"},
+      {:hackney, "~> 1.19"}
     ]
   end
 end
